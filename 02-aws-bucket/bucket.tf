b7962c75ba1862352510392a54bd38510dea78e0
Gustavo-H-Martins <gustavogalo22@hotmail.com>
2023-07-20 17:03:31 -0300

    atualização do '02-aws-bucket'

--- 02-aws-bucket/bucket.tf
+++ 02-aws-bucket/bucket.tf
@@ -1,3 +1,5 @@
+# Recurso que vamos criar na cloud AWS
 resource "aws_s3_bucket" "first_bucket" {
+  # Nome visual do recurso S3
   bucket = "curso-terraform-gustavolopes"
 }
--- 02-aws-bucket/main.tf
+++ 02-aws-bucket/main.tf
@@ -1,10 +1,10 @@
-#Este bloco define a versão mínima do Terraform e os provedores necessários para executar o código
+# Este bloco define a versão mínima do Terraform e os provedores necessários para executar o código
 terraform {
 
-  #A versão mínima do Terraform é 1.0.0
+  # A versão mínima do Terraform é 1.0.0
   required_version = ">= 1.0.0"
 
-  #O provedor AWS é necessário e deve ser da fonte hashicorp/aws e da versão 3.73.0
+  # O provedor AWS é necessário e deve ser da fonte hashicorp/aws e da versão 3.73.0
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,13 +13,13 @@ terraform {
   }
 }
 
-#Este bloco configura o provedor AWS com a região sa-east-1 e as tags padrão para todos os recursos criados
+# Este bloco configura o provedor AWS com a região sa-east-1 e as tags padrão para todos os recursos criados
 provider "aws" {
 
-  #A região onde os recursos serão criados é sa-east-1
+  # A região onde os recursos serão criados é sa-east-1
   region = "sa-east-1"
 
-  #As tags padrão para todos os recursos são owner e managed-by
+  # As tags padrão para todos os recursos são owner e managed-by
   default_tags {
     tags = {
       owner      = "gustavo.lopes"
