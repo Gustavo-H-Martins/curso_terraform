95e9a88eddca7484a6e9012da7a59c5a1d81b70e
Gustavo-H-Martins <gustavogalo22@hotmail.com>
2023-07-26 16:04:36 -0300

    26 - Local State

--- 02-aws-bucket/bucket.tf
+++ 02-aws-bucket/bucket.tf
@@ -3,3 +3,10 @@ resource "aws_s3_bucket" "first_bucket" {
   # Nome visual do recurso S3
   bucket = "curso-terraform-gustavolopes"
 }
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.first_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
